4bd5d560b50716dcf9500c984f7b442ef2db342f
mimvoid <mimvoid@proton.me>
2025-09-20 21:44:05 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Flake for Rimice, a widget system";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      allSystems = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all;
+      toSystems = passPkgs: allSystems (system: passPkgs (import nixpkgs { inherit system; }));
+    in
+    {
+      devShells = toSystems (pkgs: {
+        default = pkgs.mkShell {
+          name = "Rimice";
+
+          nativeBuildInputs = with pkgs.buildPackages; [
+            pkg-config
+            rustc
+            cargo
+            blueprint-compiler
+
+            rust-analyzer
+            rustfmt
+            clippy
+          ];
+
+          buildInputs = with pkgs.buildPackages; [
+            glib
+            gtk4
+          ];
+
+          shellHook = ''
+            export GTK_THEME=Adwaita
+          '';
+        };
+      });
+    };
+}

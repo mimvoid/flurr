2409a8b6ac92d1ea5288412c5ac3c4fa369a037d
mimvoid <mimvoid@proton.me>
2025-10-11 14:23:16 -0400

    Reorganize project directories

--- crates/flurr-dbus/src/lib.rs
+++ crates/flurr-dbus/src/lib.rs
@@ -1,12 +1,9 @@
 mod proxy;
-mod enums;
 
 pub use proxy::Application;
 pub use proxy::PinShell;
 pub use proxy::Shell;
 pub use proxy::Window;
-
-pub use enums::{Anchor, Layer, KeyboardMode};
 
 pub fn make_window_path(instance_name: &str, id: &u32) -> dbus::Path<'static> {
     dbus::Path::from(format!("/io/flurr/{instance_name}/window/{id}"))
